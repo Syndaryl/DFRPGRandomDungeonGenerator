016aa4c47e66497ddd2694c153ad65aa7026800f
Syndaryl <emily.smirle@gmail.com>
2017-10-20 15:26:11 -0400

    Cleaned up minor HTML error (p tag inside canvas, no no!) Missing ; in features.js Beginning to split Features out from Description

--- data/features.js
+++ data/features.js
@@ -1,47 +1,47 @@
 ﻿var feature = [
-  {
-    "Weight": 39,
-    "Description": "Nothing",
-    "Tables": ""
-  },
-  {
-    "Weight": 29,
-    "Description": "Random monster",
-    "Tables": "monsters"
-  },
-  {
-    "Weight": 25,
-    "Description": "Random monster plus a Treasure in a container",
-    "Tables": "monsters; treasure; container"
-  },
-  {
-    "Weight": 8,
-    "Description": "Unusual atmosphere",
-    "Tables": "atmosphere"
-  },
-  {
-    "Weight": 10,
-    "Description": "Concealed treasure",
-    "Tables": "treasure; concealment"
-  },
-  {
-    "Weight": 10,
-    "Description": "Random monster plus a concealed treasure",
-    "Tables": "monsters; treasure; concealment"
-  },
-  {
-    "Weight": 9,
-    "Description": "Weird spring or fountain",
-    "Tables": "spring"
-  },
-  {
-    "Weight": 8,
-    "Description": "Random monster plus an unusual atmosphere",
-    "Tables": "monsters; atmosphere"
-  },
-  {
-    "Weight": 8,
-    "Description": "Random monster plus a weird spring or fountain",
-    "Tables": "monsters; spring"
-  }
-]
+    {
+        "Weight": 39,
+        "Description": "Nothing",
+        "Tables": ""
+    },
+    {
+        "Weight": 29,
+        "Description": "Random monster",
+        "Tables": "monsters"
+    },
+    {
+        "Weight": 25,
+        "Description": "Random monster plus a Treasure in a container",
+        "Tables": "monsters; treasure; container"
+    },
+    {
+        "Weight": 8,
+        "Description": "Unusual atmosphere",
+        "Tables": "atmosphere"
+    },
+    {
+        "Weight": 10,
+        "Description": "Concealed treasure",
+        "Tables": "treasure; concealment"
+    },
+    {
+        "Weight": 10,
+        "Description": "Random monster plus a concealed treasure",
+        "Tables": "monsters; treasure; concealment"
+    },
+    {
+        "Weight": 9,
+        "Description": "Weird spring or fountain",
+        "Tables": "spring"
+    },
+    {
+        "Weight": 8,
+        "Description": "Random monster plus an unusual atmosphere",
+        "Tables": "monsters; atmosphere"
+    },
+    {
+        "Weight": 8,
+        "Description": "Random monster plus a weird spring or fountain",
+        "Tables": "monsters; spring"
+    }
+];
