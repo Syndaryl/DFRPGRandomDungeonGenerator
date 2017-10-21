83616eb01245447e8994bcfa71932071d7259481
Syndaryl <emily.smirle@gmail.com>
2017-10-21 11:27:04 -0400

    Oops, features table has duplicated entries, and some odd entries.

--- data/features.js
+++ data/features.js
@@ -1,15 +1,15 @@
 ﻿var feature = [
     {
-        "Weight": 39,
+        "Weight": 20,
         "Description": "Nothing"
     },
     {
-        "Weight": 29,
+        "Weight": 35,
         "Description": "Random monster",
         "Tables": "monsters"
     },
     {
-        "Weight": 25,
+        "Weight": 65,
         "Description": "Random monster plus a Treasure in a container",
         "Tables": "monsters; treasure; container"
     },
@@ -24,44 +24,34 @@
         "Tables": "treasure; concealment"
     },
     {
-        "Weight": 10,
+        "Weight": 25,
         "Description": "Random monster plus a concealed treasure",
         "Tables": "monsters; treasure; concealment"
     },
     {
-        "Weight": 9,
+        "Weight": 3,
         "Description": "Weird spring or fountain",
         "Tables": "spring"
     },
     {
-        "Weight": 8,
+        "Weight": 6,
         "Description": "Random monster plus an unusual atmosphere",
         "Tables": "monsters; atmosphere"
     },
     {
-        "Weight": 8,
+        "Weight": 6,
         "Description": "Random monster plus a weird spring or fountain",
         "Tables": "monsters; spring"
     },
     {
-        "Weight": 29,
+        "Weight": 15,
         "Description": "Trap",
         "Tables": "traps"
     },
     {
-        "Weight": 25,
+        "Weight": 20,
         "Description": "Trap plus a Treasure in a container",
         "Tables": "traps; treasure; container"
-    },
-    {
-        "Weight": 8,
-        "Description": "Unusual atmosphere",
-        "Tables": "atmosphere"
-    },
-    {
-        "Weight": 10,
-        "Description": "Concealed treasure",
-        "Tables": "treasure; concealment"
     },
     {
         "Weight": 10,
@@ -69,17 +59,12 @@
         "Tables": "traps; treasure; concealment"
     },
     {
-        "Weight": 9,
-        "Description": "Weird spring or fountain",
-        "Tables": "spring"
-    },
-    {
-        "Weight": 8,
+        "Weight": 4,
         "Description": "Trap plus an unusual atmosphere",
         "Tables": "traps; atmosphere"
     },
     {
-        "Weight": 8,
+        "Weight": 4,
         "Description": "Trap plus a weird spring or fountain",
         "Tables": "traps; spring"
     }
