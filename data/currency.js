927812b724a642df14ccfa1bf13c9169bdf2a978
Syndaryl <emily.smirle@gmail.com>
2017-10-21 13:29:23 -0400

    Broke currency out into its own file and added weight parameters.

---
+++ data/currency.js
@@ -0,0 +1,18 @@
+﻿var currency = {
+    gold: {
+        name: "gold",
+        value: 20,
+        weight: 0.02
+    },
+    silver: {
+        name: "silver",
+        value: 4,
+        weight: 0.02
+    },
+    copper: {
+        name: "copper",
+        value: 1,
+        weight: 0.02
+    }
+};
+
