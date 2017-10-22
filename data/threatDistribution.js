7f3aca1d6ea41fc281df1e136ae13938074ebf14
Syndaryl <emily.smirle@gmail.com>
2017-10-22 14:30:50 -0400

    Correcting the Threat Distribution table to use weights, N ranges, and setting up to do monsters properly.

--- data/threatDistribution.js
+++ data/threatDistribution.js
@@ -1,37 +1,205 @@
 ﻿var threat_distribution = {
     "cakewalk": {
-        "Nuisance": 50,
-        "Fodder": 15,
-        "Worthy": 5,
-        "Boss": 0,
-        "Epic": 0
+        nuisance: {
+            "Weight": 50,
+            "Description": "Nuisance",
+            N: {
+                "min": 11
+            }
+        },
+        fodder: {
+            "Weight": 15,
+            "Description": "Fodder",
+            N: {
+                "min": 3,
+                "max": 11
+            }
+        },
+        worthy: {
+            "Weight": 5,
+            "Description": "Worthy",
+            N: {
+                "min": 0.5,
+                "max": 3
+            }
+        },
+        boss: {
+            "Weight": 0,
+            "Description": "Boss",
+            N: {
+                "min": 0.1,
+                "max": 0.5
+            }
+        },
+        epic: {
+            "Weight": 0,
+            "Description": "Epic",
+            N: {
+                "max": 0.1
+            }
+        }
     },
     "easy": {
-        "Nuisance": 40,
-        "Fodder": 20,
-        "Worthy": 10,
-        "Boss": 1,
-        "Epic": 0
+        nuisance: {
+            "Weight": 40,
+            "Description": "Nuisance",
+            N: {
+                "min": 11
+            }
+        },
+        fodder: {
+            "Weight": 20,
+            "Description": "Fodder",
+            N: {
+                "min": 3,
+                "max": 11
+            }
+        },
+        worthy: {
+            "Weight": 10,
+            "Description": "Worthy",
+            N: {
+                "min": 0.5,
+                "max": 3
+            }
+        },
+        boss: {
+            "Weight": 1,
+            "Description": "Boss",
+            N: {
+                "min": 0.1,
+                "max": 0.5
+            }
+        },
+        epic: {
+            "Weight": 0,
+            "Description": "Epic",
+            N: {
+                "max": 0.1
+            }
+        }
     },
     "average": {
-        "Nuisance": 20,
-        "Fodder": 30,
-        "Worthy": 20,
-        "Boss": 1,
-        "Epic": 0
+        nuisance: {
+            "Weight": 20,
+            "Description": "Nuisance",
+            N: {
+                "min": 10,
+                "max": 20
+            }
+        },
+        fodder: {
+            "Weight": 30,
+            "Description": "Fodder",
+            N: {
+                "min": 3,
+                "max": 11
+            }
+        },
+        worthy: {
+            "Weight": 20,
+            "Description": "Worthy",
+            N: {
+                "min": 0.5,
+                "max": 3
+            }
+        },
+        boss: {
+            "Weight": 3,
+            "Description": "Boss",
+            N: {
+                "min": 0.1,
+                "max": 0.5
+            }
+        },
+        epic: {
+            "Weight": 0,
+            "Description": "Epic",
+            N: {
+                "max": 0.1
+            }
+        }
     },
     "hard": {
-        "Nuisance": 10,
-        "Fodder": 15,
-        "Worthy": 40,
-        "Boss": 5,
-        "Epic": 0
+        nuisance: {
+            "Weight": 10,
+            "Description": "Nuisance",
+            N: {
+                "min": 10,
+                "max": 20
+            }
+        },
+        fodder: {
+            "Weight": 15,
+            "Description": "Fodder",
+            N: {
+                "min": 3,
+                "max": 11
+            }
+        },
+        worthy: {
+            "Weight": 40,
+            "Description": "Worthy",
+            N: {
+                "min": 0.5,
+                "max": 3
+            }
+        },
+        boss: {
+            "Weight": 5,
+            "Description": "Boss",
+            N: {
+                "min": 0.1,
+                "max": 0.5
+            }
+        },
+        epic: {
+            "Weight": 1,
+            "Description": "Epic",
+            N: {
+                "max": 0.1
+            }
+        }
     },
     "evil": {
-        "Nuisance": 0,
-        "Fodder": 10,
-        "Worthy": 35,
-        "Boss": 25,
-        "Epic": 1
+        nuisance: {
+            "Weight": 0,
+            "Description": "Nuisance",
+            N: {
+                "min": 10,
+                "max": 20
+            }
+        },
+        fodder: {
+            "Weight": 2,
+            "Description": "Fodder",
+            N: {
+                "min": 3,
+                "max": 11
+            }
+        },
+        worthy: {
+            "Weight": 7,
+            "Description": "Worthy",
+            N: {
+                "min": 0.5,
+                "max": 3
+            }
+        },
+        boss: {
+            "Weight": 7,
+            "Description": "Boss",
+            N: {
+                "min": 0.1,
+                "max": 0.5
+            }
+        },
+        epic: {
+            "Weight": 2,
+            "Description": "Epic",
+            N: {
+                "max": 0.1
+            }
+        }
     }
 };
