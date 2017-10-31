35a480e4eff475412c56fcc867eebe2f1694d8f2
Syndaryl <emily.smirle@gmail.com>
2017-10-31 19:09:43 -0400

    Redo from start on encounters - pick pick something of our CER or less, get a few to make up an encounter around the dungeons CER.

--- data/features.js
+++ data/features.js
@@ -1,71 +1,11 @@
 ﻿var feature = [
     {
-        "Weight": 20,
+        "Weight": 1,
         "Description": "Nothing"
     },
     {
         "Weight": 35,
         "Description": "Random monster",
         "Tables": "monsters"
-    },
-    {
-        "Weight": 65,
-        "Description": "Random monster plus a Treasure in a container",
-        "Tables": "monsters; treasure; container"
-    },
-    {
-        "Weight": 8,
-        "Description": "Unusual atmosphere",
-        "Tables": "atmosphere"
-    },
-    {
-        "Weight": 10,
-        "Description": "Concealed treasure",
-        "Tables": "treasure; concealment"
-    },
-    {
-        "Weight": 25,
-        "Description": "Random monster plus a concealed treasure",
-        "Tables": "monsters; treasure; concealment"
-    },
-    {
-        "Weight": 3,
-        "Description": "Weird spring or fountain",
-        "Tables": "spring"
-    },
-    {
-        "Weight": 6,
-        "Description": "Random monster plus an unusual atmosphere",
-        "Tables": "monsters; atmosphere"
-    },
-    {
-        "Weight": 6,
-        "Description": "Random monster plus a weird spring or fountain",
-        "Tables": "monsters; spring"
-    },
-    {
-        "Weight": 15,
-        "Description": "Trap",
-        "Tables": "traps"
-    },
-    {
-        "Weight": 20,
-        "Description": "Trap plus a Treasure in a container",
-        "Tables": "traps; treasure; container"
-    },
-    {
-        "Weight": 10,
-        "Description": "Trap plus a concealed treasure",
-        "Tables": "traps; treasure; concealment"
-    },
-    {
-        "Weight": 4,
-        "Description": "Trap plus an unusual atmosphere",
-        "Tables": "traps; atmosphere"
-    },
-    {
-        "Weight": 4,
-        "Description": "Trap plus a weird spring or fountain",
-        "Tables": "traps; spring"
     }
 ];
