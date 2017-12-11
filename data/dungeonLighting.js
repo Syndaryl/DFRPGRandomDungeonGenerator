249f9bb27559891359ebfca188e2e343c76fd12a
Syndaryl <Syndaryl@users.noreply.github.com>
2017-12-11 09:35:29 -0500

    Setting up for Dungeon Lighting

---
+++ data/dungeonLighting.js
@@ -0,0 +1,268 @@
+dungeonLighting = [
+    {
+      "Description": "Daylight",
+      "Penalty": "0",
+      "Weight": "0",
+      "Tags": [
+        "Outdoors",
+        "NoRoof"
+      ]
+    },
+    {
+      "Description": "Near Sunset",
+      "Penalty": "-1",
+      "Weight": "0",
+      "Tags": [
+        "Outdoors",
+        "NoRoof"
+      ]
+    },
+    {
+      "Description": "Twilight",
+      "Penalty": "-2",
+      "Weight": "0",
+      "Tags": [
+        "Outdoors",
+        "NoRoof"
+      ]
+    },
+    {
+      "Description": "Twilight",
+      "Penalty": "-3",
+      "Weight": "0",
+      "Tags": [
+        "Outdoors",
+        "NoRoof"
+      ]
+    },
+    {
+      "Description": "Moonlight",
+      "Penalty": "-4",
+      "Weight": "0",
+      "Tags": [
+        "Outdoors",
+        "NoRoof"
+      ]
+    },
+    {
+      "Description": "Moonlight",
+      "Penalty": "-5",
+      "Weight": "0",
+      "Tags": [
+        "Outdoors",
+        "NoRoof"
+      ]
+    },
+    {
+      "Description": "Moonlight",
+      "Penalty": "-6",
+      "Weight": "0",
+      "Tags": [
+        "Outdoors",
+        "NoRoof"
+      ]
+    },
+    {
+      "Description": "Moonless night",
+      "Penalty": "-7",
+      "Weight": "0",
+      "Tags": [
+        "Outdoors",
+        "NoRoof"
+      ]
+    },
+    {
+      "Description": "Glowing Slime",
+      "Penalty": "-8",
+      "Weight": "0",
+      "Tags": [
+        "Outdoors",
+        "NoRoof",
+        "Slime",
+        "Dungeon",
+        "Cave",
+        "Uninhabited",
+        "Inhabited"
+      ]
+    },
+    {
+      "Description": "Total Darkness",
+      "Penalty": "-10",
+      "Weight": "5",
+      "Tags": [
+        "Dungeon",
+        "Cave",
+        "Uninhabited",
+        "Inhabited"
+      ]
+    },
+    {
+      "Description": "Torches at 3 yard intervals",
+      "Penalty": "-3",
+      "Weight": "5",
+      "Tags": [
+        "Outoors",
+        "Dungeon",
+        "Cave",
+        "Inhabited",
+        "NoRoof"
+      ]
+    },
+    {
+      "Description": "Large Candelabras at 3 yard intervals",
+      "Penalty": "-3",
+      "Weight": "2",
+      "Tags": [
+        "Dungeon",
+        "Cave",
+        "Inhabited"
+      ]
+    },
+    {
+      "Description": "Candelabras at 3 yard intervals",
+      "Penalty": "-5",
+      "Weight": "3",
+      "Tags": [
+        "Dungeon",
+        "Cave",
+        "Inhabited"
+      ]
+    },
+    {
+      "Description": "Pillar candles at 3 yard intervals",
+      "Penalty": "-5",
+      "Weight": "5",
+      "Tags": [
+        "Dungeon",
+        "Cave",
+        "Inhabited"
+      ]
+    },
+    {
+      "Description": "Candles at 3 yard intervals",
+      "Penalty": "-7",
+      "Weight": "3",
+      "Tags": [
+        "Dungeon",
+        "Cave",
+        "Inhabited"
+      ]
+    },
+    {
+      "Description": "Small candles at 3 yard intervals",
+      "Penalty": "-9",
+      "Weight": "1",
+      "Tags": [
+        "Dungeon",
+        "Cave",
+        "Inhabited"
+      ]
+    },
+    {
+      "Description": "Lanterns at 3 yard Intervals",
+      "Penalty": "-3",
+      "Weight": "5",
+      "Tags": [
+        "Outoors",
+        "Dungeon",
+        "Cave",
+        "Inhabited",
+        "NoRoof"
+      ]
+    },
+    {
+      "Description": "Permanant Sunlight Spell",
+      "Penalty": "0",
+      "Weight": "1",
+      "Tags": [
+        "Outdoors",
+        "NoRoof",
+        "Dungeon",
+        "Cave",
+        "Uninhabited",
+        "Inhabited"
+      ]
+    },
+    {
+      "Description": "Diffuse magical Glow",
+      "Penalty": "-2",
+      "Weight": "3",
+      "Tags": [
+        "Outdoors",
+        "NoRoof",
+        "Dungeon",
+        "Cave",
+        "Uninhabited",
+        "Inhabited"
+      ]
+    },
+    {
+      "Description": "Bright glowing crystals embedded in the walls and ceilings",
+      "Penalty": "-1",
+      "Weight": "1",
+      "Tags": [
+        "Dungeon",
+        "Cave",
+        ""
+      ]
+    },
+    {
+      "Description": "Bright glowing mushrooms in clusters at the edges of floors",
+      "Penalty": "-1",
+      "Weight": "2",
+      "Tags": [
+        "Outdoors",
+        "NoRoof",
+        "Dungeon",
+        "Cave",
+        "Uninhabited",
+        "Inhabited"
+      ]
+    },
+    {
+      "Description": "Glowing crystals embedded in the walls and ceilings",
+      "Penalty": "-4",
+      "Weight": "3",
+      "Tags": [
+        "Dungeon",
+        "Cave",
+        "Uninhabited",
+        "Inhabited"
+      ]
+    },
+    {
+      "Description": "Glowing mushrooms in clusters at the edges of floors",
+      "Penalty": "-4",
+      "Weight": "4",
+      "Tags": [
+        "Dungeon",
+        "Cave",
+        "Uninhabited",
+        "Inhabited"
+      ]
+    },
+    {
+      "Description": "Dimly glowing crystals embedded in the walls and ceilings",
+      "Penalty": "-6",
+      "Weight": "5",
+      "Tags": [
+        "Dungeon",
+        "Cave",
+        "Uninhabited",
+        "Inhabited"
+      ]
+    },
+    {
+      "Description": "Dimly glowing mushrooms in clusters at the edges of floors",
+      "Penalty": "-6",
+      "Weight": "5",
+      "Tags": [
+        "Outdoors",
+        "NoRoof",
+        "Dungeon",
+        "Cave",
+        "Uninhabited",
+        "Inhabited"
+      ]
+    }
+  ];
