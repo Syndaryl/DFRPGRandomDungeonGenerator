ae4d48abf82d0760e6f7dfc17fa466b6cdc69c66
Syndaryl <emily.smirle@gmail.com>
2017-12-12 15:46:10 -0500

    Fixed the lighting. Again. Deja vu, I did this all yesterday.

--- data/roomLighting.js
+++ data/roomLighting.js
@@ -1,6 +1,6 @@
 var roomLighting = [
     {
-      "Light level": "Glowing Slime",
+      "Description": "Glowing Slime",
       "Penalty": "-8",
       "Weight": "0",
       "Tags": [
@@ -14,7 +14,7 @@ var roomLighting = [
       ]
     },
     {
-      "Light level": "Total Darkness",
+      "Description": "Total Darkness",
       "Penalty": "-10",
       "Weight": "5",
       "Tags": [
@@ -25,7 +25,7 @@ var roomLighting = [
       ]
     },
     {
-      "Light level": "Torches at 3 yard intervals",
+      "Description": "Torches at 3 yard intervals",
       "Penalty": "-3",
       "Weight": "5",
       "Tags": [
@@ -37,7 +37,7 @@ var roomLighting = [
       ]
     },
     {
-      "Light level": "Lanterns at 3 yard Intervals",
+      "Description": "Lanterns at 3 yard Intervals",
       "Penalty": "-3",
       "Weight": "5",
       "Tags": [
@@ -49,7 +49,7 @@ var roomLighting = [
       ]
     },
     {
-      "Light level": "Permanant Sunlight Spell",
+      "Description": "Permanant Sunlight Spell",
       "Penalty": "0",
       "Weight": "5",
       "Tags": [
@@ -62,7 +62,7 @@ var roomLighting = [
       ]
     },
     {
-      "Light level": "Diffuse magical Glow",
+      "Description": "Diffuse magical Glow",
       "Penalty": "-2",
       "Weight": "5",
       "Tags": [
@@ -75,7 +75,7 @@ var roomLighting = [
       ]
     },
     {
-      "Light level": "Large Candelabras at 3 yard intervals",
+      "Description": "Large Candelabras at 3 yard intervals",
       "Penalty": "-3",
       "Weight": "2",
       "Tags": [
@@ -85,7 +85,7 @@ var roomLighting = [
       ]
     },
     {
-      "Light level": "Candelabras at 3 yard intervals",
+      "Description": "Candelabras at 3 yard intervals",
       "Penalty": "-5",
       "Weight": "3",
       "Tags": [
@@ -95,7 +95,7 @@ var roomLighting = [
       ]
     },
     {
-      "Light level": "Pillar candles at 3 yard intervals",
+      "Description": "Pillar candles at 3 yard intervals",
       "Penalty": "-5",
       "Weight": "5",
       "Tags": [
@@ -105,7 +105,7 @@ var roomLighting = [
       ]
     },
     {
-      "Light level": "Candles at 3 yard intervals",
+      "Description": "Candles at 3 yard intervals",
       "Penalty": "-7",
       "Weight": "3",
       "Tags": [
@@ -115,7 +115,7 @@ var roomLighting = [
       ]
     },
     {
-      "Light level": "Small candles at 3 yard intervals",
+      "Description": "Small candles at 3 yard intervals",
       "Penalty": "-9",
       "Weight": "1",
       "Tags": [
