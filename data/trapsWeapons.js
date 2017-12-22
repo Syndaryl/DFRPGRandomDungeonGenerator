b6bd02b03d10f03f5dd94406302c4513bcacdf2f
Syndaryl <Syndaryl@users.noreply.github.com>
2017-12-22 14:30:29 -0500

    Preparation for doing more fancy stuff with weapons.

---
+++ data/trapsWeapons.js
@@ -0,0 +1,205 @@
+var trapsWeaponsST = {
+basic: [
+    {
+      "Description": "ST 10",
+      "Weight": 3
+    },
+    {
+      "Description": "ST 14",
+      "Weight": 2
+    },
+    {
+      "Description": "ST 18",
+      "Weight": 1
+    },
+    {
+      "Description": "ST 22",
+      "Weight": 1
+    },
+    {
+      "Description": "ST 26",
+      "Weight": 0
+    }
+],
+standard: [
+    {
+      "Description": "ST 10",
+      "Weight": 2
+    },
+    {
+      "Description": "ST 14",
+      "Weight": 4
+    },
+    {
+      "Description": "ST 18",
+      "Weight": 3
+    },
+    {
+      "Description": "ST 22",
+      "Weight": 2
+    },
+    {
+      "Description": "ST 26",
+      "Weight": 1
+    }
+],
+secure: [
+    {
+      "Description": "ST 10",
+      "Weight": 1
+    },
+    {
+      "Description": "ST 14",
+      "Weight": 2
+    },
+    {
+      "Description": "ST 18",
+      "Weight": 5
+    },
+    {
+      "Description": "ST 22",
+      "Weight": 3
+    },
+    {
+      "Description": "ST 26",
+      "Weight": 2
+    }
+],
+deathtrap: [
+    {
+      "Description": "ST 10",
+      "Weight": 0
+    },
+    {
+      "Description": "ST 14",
+      "Weight": 1
+    },
+    {
+      "Description": "ST 18",
+      "Weight": 2
+    },
+    {
+      "Description": "ST 22",
+      "Weight": 5
+    },
+    {
+      "Description": "ST 26",
+      "Weight": 3
+    }
+]
+};
+
+
+var trapsWeapons = [
+    {
+      "Description": "Broadsword",
+      "ST 10": "1d+1",
+      "ST 14": "2d+1",
+      "ST 18": "3d+1",
+      "ST 22": "4d+1",
+      "ST 26": "5d+1",
+      "Type": "cutting",
+      "Melee": false,
+      "Ranged": true,
+      "Weight": 5
+    },
+    {
+      "Description": "Crossbow",
+      "ST 10": "1d+2",
+      "ST 14": "1d+4",
+      "ST 18": "1d+6",
+      "ST 22": "2d+4",
+      "ST 26": "2d+6",
+      "Type": "impaling",
+      "Melee": true,
+      "Ranged": false,
+      "Weight": 5
+    },
+    {
+      "Description": "Great Axe",
+      "ST 14": "2d+4",
+      "ST 18": "3d+4",
+      "ST 22": "4d+4",
+      "ST 26": "5d+4",
+      "Type": "cutting",
+      "Melee": false,
+      "Ranged": true,
+      "Weight": 5
+    },
+    {
+      "Description": "Halberd",
+      "ST 14": "2d+5",
+      "ST 18": "3d+5",
+      "ST 22": "4d+5",
+      "ST 26": "5d+5",
+      "Type": "cutting",
+      "Melee": false,
+      "Ranged": true,
+      "Weight": 5
+    },
+    {
+      "Description": "Large Knife",
+      "ST 10": "1d-2",
+      "ST 14": "1d",
+      "ST 18": "1d+2",
+      "Type": "impaling",
+      "Melee": true,
+      "Ranged": true,
+      "Weight": 5
+    },
+    {
+      "Description": "Longbow",
+      "ST 10": "1d",
+      "ST 14": "1d+2",
+      "ST 18": "1d+4",
+      "ST 22": "2d+2",
+      "ST 26": "2d+4",
+      "Type": "impaling",
+      "Melee": true,
+      "Ranged": false,
+      "Weight": 5
+    },
+    {
+      "Description": "Maul",
+      "ST 14": "2d+5",
+      "ST 18": "3d+5",
+      "ST 22": "4d+5",
+      "ST 26": "5d+5",
+      "Type": "crushing",
+      "Melee": false,
+      "Ranged": true,
+      "Weight": 5
+    },
+    {
+      "Description": "Pollaxe",
+      "ST 14": "2d+3",
+      "ST 18": "3d+3",
+      "ST 22": "4d+3",
+      "ST 26": "5d+3",
+      "Type": "impaling",
+      "Melee": false,
+      "Ranged": true,
+      "Weight": 5
+    },
+    {
+      "Description": "Small Mace",
+      "ST 10": "1d+2",
+      "ST 14": "2d+2",
+      "ST 18": "3d+2",
+      "ST 22": "4d+2",
+      "ST 26": "5d+2",
+      "Type": "crushing",
+      "Melee": true,
+      "Ranged": true,
+      "Weight": 5
+    },
+    {
+      "Description": "Star Shuriken",
+      "ST 10": "1d-3",
+      "ST 14": "1d-1",
+      "Type": "cutting",
+      "Melee": true,
+      "Ranged": false,
+      "Weight": 5
+    }
+   ];
