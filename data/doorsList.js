afc8f4d415828c9eb147fb7df66f2f6f4c4f19f3
Syndaryl <Syndaryl@users.noreply.github.com>
2018-02-05 14:11:57 -0500

    Infrastructure for door descriptions

---
+++ data/doorsList.js
@@ -0,0 +1,102 @@
+var doorsList = [
+ {
+   "Weight": 4,
+   "Description": "Pine door (1\"): DR 1; HP 23",
+   "Tags": "Wood"
+ },
+ {
+   "Weight": 9,
+   "Description": "Ironbound pine door (1\"): DR 5; HP 27",
+   "Tags": "Ironbound. Wood"
+ },
+ {
+   "Weight": 9,
+   "Description": "Iron door (1/16\"): DR 12; HP 36",
+   "Tags": "Iron"
+ },
+ {
+   "Weight": 90,
+   "Description": "Oak door (1/2\"): DR 2; HP 29",
+   "Tags": "Wood"
+ },
+ {
+   "Weight": 26,
+   "Description": "Ironbound oak door (1/2\"): DR 10; HP 34",
+   "Tags": "Ironbound. Wood"
+ },
+ {
+   "Weight": 9,
+   "Description": "Iron (1/8\"): DR 25; HP 46 - 190 lbs; ST 6 needed to open",
+   "Tags": "Iron"
+ },
+ {
+   "Weight": 60,
+   "Description": "Oak door (1\"): DR 3; HP 33",
+   "Tags": "Wood"
+ },
+ {
+   "Weight": 55,
+   "Description": "Ironbound oak door (1\"): DR 15; HP 39",
+   "Tags": "Ironbound. Wood"
+ },
+ {
+   "Weight": 9,
+   "Description": "Iron door (1/4\"): DR 50; HP 58 - 375 lbs; ST 9 needed to open",
+   "Tags": "Iron"
+ },
+ {
+   "Weight": 14,
+   "Description": "Oak door (2\"): DR 6; HP 42",
+   "Tags": "Wood"
+ },
+ {
+   "Weight": 24,
+   "Description": "Ironbound oak door (2\"): DR 30; HP 49 - 225 lbs; ST 7 needed to open",
+   "Tags": "Ironbound. Wood"
+ },
+ {
+   "Weight": 9,
+   "Description": "Iron door (1/2\"): DR 75; HP 66 - 600 lbs; ST 11 needed to open",
+   "Tags": "Iron"
+ },
+ {
+   "Weight": 4,
+   "Description": "Oak vault door (4\"): DR 12; HP 54 - 300 lbs; ST 8 needed to open",
+   "Tags": "Wood"
+ },
+ {
+   "Weight": 4,
+   "Description": "Ironbound oak vault door (4\"): DR 60; HP 62 - 420 lbs; ST 9 needed to open",
+   "Tags": "Ironbound. Wood"
+ },
+ {
+   "Weight": 9,
+   "Description": "Iron vault door (1.5\"): DR 150; HP 84 - 1200 lbs; ST 16 needed to open",
+   "Tags": "Iron"
+ },
+ {
+   "Weight": 9,
+   "Description": "Thin Stone door (1.5\"): DR 4*; HP 74 - 800 lbs; ST 13 needed to open",
+   "Tags": "Stone"
+ },
+ {
+   "Weight": 9,
+   "Description": "Average stone door (3\"): DR 8*; HP 93 - 1600 lbs; ST 18 needed to open",
+   "Tags": "Stone"
+ },
+ {
+   "Weight": 9,
+   "Description": "Heavy stone door (6\"): DR 16*; HP 118 - 3200 lbs; ST 26 needed to open",
+   "Tags": "Stone"
+ },
+ {
+   "Weight": 9,
+   "Description": "Extremely heavy stone door (9\"): DR 24*; HP 135 - 4800 lbs; ST 31 needed to open",
+   "Tags": "Stone"
+ },
+ {
+   "Weight": 9,
+   "Description": "Stone vault door (18\"): DR 48*; HP 170 - 9600 lbs; ST 44 needed to open",
+   "Tags": "Stone"
+ }
+];
