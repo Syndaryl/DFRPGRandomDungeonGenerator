9495b2efe0854efe63f0e28cc8ce33fe01076dd3
Syndaryl <Syndaryl@users.noreply.github.com>
2018-03-01 13:00:16 -0500

    Expanded/elaborated on concealment

--- data/concealment.js
+++ data/concealment.js
@@ -1,30 +1,58 @@
 ﻿var concealment = [
   {
-    "Weight": 9,
-    "Description": "Concealed behind a tapestry or furniture: Search -1"
+    "Weight": 5,
+    "Description": "Concealed behind a tapestry: Search -1"
   },
   {
-    "Weight": 24,
+    "Weight": 5,
+    "Description": "Concealed behind furniture: Search -1"
+  },
+  {
+    "Weight": 14,
     "Description": "Simply concealed in room construction: Search -2"
   },
   {
     "Weight": 29,
-    "Description": "Simply concealed in room construction, and behind decorations or furniture: Search -3"
+    "Description": "Simply concealed in room construction, and behind tapestries: Search -3"
+  },
+  {
+    "Weight": 25,
+    "Description": "Simply concealed in room construction, and behind furniture: Search -3"
   },
   {
     "Weight": 24,
-    "Description": "Expertly concealed in room construction (swinging book-case/mantle): Search -4"
-  },
-  {
-    "Weight": 14,
-    "Description": "Expertly concealed in wall construction, and behind decorations or furniture: Search -6"
+    "Description": "Expertly concealed in room construction (e.g. swinging book-case/fireplace mantle): Search -4"
   },
   {
     "Weight": 9,
-    "Description": "Masterfully concealed in wall construction, behind wall decorations: Search -8"
+    "Description": "Expertly concealed in wall construction, and behind tapestries: Search -6"
+  },
+  {
+    "Weight": 9,
+    "Description": "Expertly concealed in wall construction, and behind a relief sculpture: Search -6"
+  },
+  {
+    "Weight": 14,
+    "Description": "Expertly concealed in wall construction, and behind furniture: Search -6"
+  },
+  {
+    "Weight": 9,
+    "Description": "Masterfully concealed in wall construction (rotating wall segment), behind tapestries: Search -8"
+  },
+  {
+    "Weight": 9,
+    "Description": "Masterfully concealed in wall construction (rotating wall segment), behind furniture: Search -8"
+  },
+  {
+    "Weight": 2,
+    "Description": "Masterfully concealed in wall construction (rotating wall segment), behind an elaborate artistic relief: Search -10"
+  },
+  {
+    "Weight": 2,
+    "Description": "Masterfully concealed in wall construction (rotating wall segment), in an elaborate wall fresco: Search -10"
   },
   {
     "Weight": 4,
-    "Description": "Masterfully concealed in wall construction, behind elaborate wall decorations or large, heavy furniture: Search -10"
+    "Description": "Masterfully concealed in wall construction (rotating wall segment), behind large, heavy furniture: Search -10"
   }
 ];
