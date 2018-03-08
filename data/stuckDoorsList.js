7b49705a5a3cfc76785fa2128f0f8f136f33bf0a
Syndaryl <Syndaryl@users.noreply.github.com>
2018-03-08 15:29:42 -0500

    Multifeature doors
    Doors can now be locked AND trapped AND stuck, hurray!
    TODO: Details on locked and trapped.

---
+++ data/stuckDoorsList.js
@@ -0,0 +1,18 @@
+var stuckDoors = [
+    {
+      "Weight": 65,
+      "Description": "It is a bit stuck (ST roll to force open)"
+    },
+    {
+        "Weight": 35,
+        "Description": "It is stuck (ST-2 roll to force open)"
+    },
+    {
+        "Weight": 25,
+        "Description": "It is really stuck (ST-4 roll to force open)"
+    },
+    {
+        "Weight": 1,
+        "Description": "It is seriously jamned (ST-6 roll to force open)"
+    }
+   ];
