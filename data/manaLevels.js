71fb806afcc4ffa5aa0176d8238d89c3bab5217e
Syndaryl <emily.smirle@gmail.com>
2018-03-11 09:12:15 -0400

    Add mana levels table
    Also useful for sanctity, but doesn't handle Nature's Strength

---
+++ data/manaLevels.js
@@ -0,0 +1,22 @@
+var manaLevels = [
+    {
+        "Weight": 4,
+        "Description": "Very High"
+    },
+    {
+        "Weight": 20,
+        "Description": "High"
+    },
+    {
+        "Weight": 200,
+        "Description": "Normal"
+    },
+    {
+        "Weight": 50,
+        "Description": "Low"
+    },
+    {
+        "Weight": 1,
+        "Description": "No"
+    }
+];
