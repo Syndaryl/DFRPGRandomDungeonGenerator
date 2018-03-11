cedab2fb23c81126e0eecd692a52347677101028
Syndaryl <emily.smirle@gmail.com>
2018-03-11 09:30:39 -0400

    Mana and Sanctity implemented

--- data/manaLevels.js
+++ data/manaLevels.js
@@ -1,22 +1,27 @@
-var manaLevels = [
+﻿var manaLevels = [
     {
         "Weight": 4,
-        "Description": "Very High"
+        "Description": "Very High",
+        "Penalty": "Recover personal FP spent on spells a second later; regular failure counts as critical failure, critical failure is disastrous."
     },
     {
         "Weight": 20,
-        "Description": "High"
+        "Description": "High",
+        "Penalty": "Recover Energy restores 1 FP/5 minutes at any skill level, 1 FP/2 minutes at skill 15‑19, 1 FP/minute at skill 20+."
     },
     {
-        "Weight": 200,
-        "Description": "Normal"
+        "Weight": 150,
+        "Description": "Normal",
+        "Penalty": "No special rules"
     },
     {
         "Weight": 50,
-        "Description": "Low"
+        "Description": "Low",
+        "Penalty": "Spells at -5 skill. Recover Energy doesn't work!"
     },
     {
         "Weight": 1,
-        "Description": "No"
+        "Description": "No",
+        "Penalty": "Magic doesn't work!"
     }
 ];
