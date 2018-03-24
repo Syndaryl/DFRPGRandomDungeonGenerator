2842fc61e2611872fd08d5f691fec18ccf11cb6e
Syndaryl <emily.smirle@gmail.com>
2018-03-24 16:19:35 -0400

    Tables

---
+++ data/travel_times.js
@@ -0,0 +1,30 @@
+﻿var travel_times = {
+    "town": {
+        "Weight": 1,
+        "Description": "In the Neighborhood",
+        "Time": "1d6-3",
+        "Treasure": 0.8,
+        "Notes": "Everone knows a little something about these places! Proximity to town means significantly less risk for delvers and the site as probably been picked over before"
+    },
+    "near": {
+        "Weight": 1,
+        "Description": "Next Dungeon Over",
+        "Time": "1d6+2",
+        "Treasure": 1,
+        "Notes": "Returning to town mid-delve is inconvenient but not impossible."
+    },
+    "average": {
+        "Weight": 1,
+        "Description": "Guys, I Think We're Lost",
+        "Time": "2d6+4",
+        "Treasure": 1.5,
+        "Notes": "Travel is more difficult on this scale: Navigation and Weather Sense rolls are at -2. On the bright side, with greater time spent comes greater return on investment!"
+    },
+    "far": {
+        "Weight": 1,
+        "Description": "I Don't Want To Take The Hobbits To Isengard",
+        "Time": "4d6+16",
+        "Treasure": 2.5,
+        "Notes": "Travel at these distances is a real challenge: Navigation and Weather Sense rolls are at -5. This is conceivably a full season's round trip, and let's face it, nobody else has ever looted this dungeon!"
+    }
+};
