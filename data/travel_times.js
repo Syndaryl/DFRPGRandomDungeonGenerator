cb77bc0c1d35459aded7c2f86099d560b32fce12
Syndaryl <Syndaryl@users.noreply.github.com>
2018-05-29 15:16:28 -0400

    Random Monster table fix, added random travel times
    Removed horribe kludge fix to the random monster table generator running over 100 by rewriting the function to do two passes, like a sane person.
    Added the option to randomly pick travel times.
    Added more documentation

--- data/travel_times.js
+++ data/travel_times.js
@@ -1,30 +1,19 @@
-﻿var travel_times = {
-    "town": {
-        "Weight": 1,
-        "Description": "In the Neighborhood",
-        "Time": "1d6-3",
-        "Treasure": 0.8,
-        "Notes": "Everone knows a little something about these places! Proximity to town means significantly less risk for delvers and the site has probably been picked over before."
+﻿var travel_times = [
+    {
+        Type: "short",
+        Weight: 7
     },
-    "near": {
-        "Weight": 1,
-        "Description": "Next Dungeon Over",
-        "Time": "1d6+2",
-        "Treasure": 1,
-        "Notes": "Returning to town mid-delve is inconvenient but not impossible."
+    
+    {
+        Type: "mid",
+        Weight: 25
     },
-    "average": {
-        "Weight": 1,
-        "Description": "Guys, I Think We're Lost",
-        "Time": "2d6+4",
-        "Treasure": 1.5,
-        "Notes": "Travel is more difficult on this scale: Navigation and Weather Sense rolls are at -2. On the bright side, with greater time spent comes greater return on investment!"
+    {
+        Type: "long",
+        Weight: 15
     },
-    "far": {
-        "Weight": 1,
-        "Description": "I Don't Want To Take The Hobbits To Isengard",
-        "Time": "4d6+16",
-        "Treasure": 2.5,
-        "Notes": "Travel at these distances is a real challenge: Navigation and Weather Sense rolls are at -5. This is conceivably a full season's round trip, and let's face it, nobody else has ever looted this dungeon!"
+    {
+        Type: "super_far",
+        Weight: 1
     }
-};
+];
