43c529e7062049e52ea6b295ccb7368e748dea9f
Syndaryl <emily.smirle@gmail.com>
2018-06-02 16:19:43 -0400

    Biome list
    So we can describe what kind of terrain you have to travel through (and what Survival specialty applies)

---
+++ data/biomes.js
@@ -0,0 +1,10 @@
+﻿var biomes = [
+    {"Description": "Arctic", "Weight": 2},
+    {"Description": "Desert", "Weight": 2},
+    {"Description": "Island/Beach", "Weight": 2},
+    {"Description": "Jungle", "Weight": 2},
+    {"Description": "Mountain", "Weight": 2},
+    {"Description": "Plains", "Weight": 2},
+    {"Description": "Swampland", "Weight": 2},
+    {"Description": "Woodlands", "Weight": 2}
+];
